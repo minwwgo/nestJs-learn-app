e6f5af03d0a68ba585d3892c98527124bd4e5f36
MINKO-MBF <minko@maharbawgamoney.com>
2025-07-27 16:30:15 +0700

    Day 1: TypeScript basiscs done

---
+++ day1.js
@@ -0,0 +1,16 @@
+// Variables and Types
+var name = "Alice";
+var age = 7;
+var isStudent = true;
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student?", isStudent);
+// Array
+var fruits = ["apple", "banana", "mango"];
+console.log("Fruits:", fruits);
+// Object
+var user = {
+    name: "Ko Ko",
+    age: 25
+};
+console.log("User:", user);
---
+++ day1.ts
@@ -0,0 +1,19 @@
+// Variables and Types
+const name: string = "Alice";
+const age: number = 7;
+const isStudent: boolean = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student?", isStudent);
+
+// Array
+const fruits: string[] = ["apple", "banana", "mango"];
+console.log("Fruits:", fruits);
+
+// Object
+const user: { name: string; age: number } = {
+  name: "Ko Ko",
+  age: 25
+};
+console.log("User:", user);
